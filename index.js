468e8d727aa9cd94afeb6b3475af15ee991aaaf3
Samuel <smarouba2@gmail.com>
2022-06-02 14:58:44 -0500

    commit tree b917583deebdc06f5c6bdfe8a17de2522cfa986b

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    if (saturdayFun) {
+    return `This Saturday, I want to ${activity}!`
+} 
+}
+
+function mondayWork (activity = 'go to the office') {
+    if (mondayWork) {
+    return `This Monday, I will ${activity}.`;
+    }
+}
+function wrapAdjective(wrapper = "*") {
+    return function innerFunction(def = "special") {
+        return `You are ${wrapper}${def}${wrapper}!`
+    }
+}
+
+
+
+// describe("defines wrapAdjective function according to the specification", function() {
+//     it("function exists", function() {
+//       expect(wrapAdjective).to.exist
+//     })
+    
+//     it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective('*')
+//       let emphatic = result("a hard worker")
+//       expect(emphatic).to.equal("You are *a hard worker*!")
+//     });
+
+//     it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective("||")
+//       let emphatic = result("a dedicated programmer")
+//       expect(emphatic).to.equal("You are ||a dedicated programmer||!")
+//     });
+//   })
+
+// (function (baseNumber) {
+//     return baseNumber + 3;
+// })(2); // two acts as a parameter? also 2 = baseNumbher underIIFE
+
+
+
